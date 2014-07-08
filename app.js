128a3dbf38de7ffb102cbb8964ab41b899cd07a4
Nathan <nathansherburn@gmail.com>
2014-07-08 11:14:07 +1000

    Initial code commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var connect = require('connect')
+var CAS = require('cas')
+var path = require('path')
+var cas = new CAS({base_url: 'https://my.monash.edu.au/authentication/cas', service: 'http://melts-dev.eng.monash.edu'})
+var app = express()
+
+app.set("views", __dirname + "/templates");
+app.engine('html', require('hogan-express'))
+app.set('view engine', 'html')			//# use .html extension for templates
+app.set('layout', 'layout')				//# use layout.html as the default layout
+// app.set('partials', {foo: 'foo'})	//# define partials available to all pages
+// app.enable('view cache')
+
+
+app.use(connect.urlencoded()) //for getting post information such as forms
+app.use(express.static(path.join(__dirname, 'public'))) //for serving static files such as css
+
+
+app.get('/', function (req, res) {
+	console.log(req.param('ticket'))
+	var ticket = req.param('ticket')
+	if (ticket) {
+		cas.validate(ticket, function(err, status, username) {
+			if (err) {
+		        // Handle the error
+		        console.log('there was an error')
+		        res.send({error: err})
+		    } else {
+		        // Log the user in
+		        console.log('the user was logged in')
+		        res.send({status: status, username: username})
+		    }
+		})
+	} else {
+		res.redirect('/login')
+	}
+})
+
+app.get('/quiz', function (req, res) {
+	res.render('quiz', {
+		'question': 'hello',
+		'answer1': 'the goat',
+		'answer2': 'the goat',
+		'answer3': 'the goat',
+		'answer4': 'the goat'	
+	})
+})
+
+app.get('/login', function (req, res) {
+	res.render('login')
+})
+
+// app.post('/home', function (req, res) {
+// 	// res.end('your username is ' + req.body.username + " and your password is " + req.body.password)
+// 	res.render('home', {username: req.body.username, password: req.body.password})
+// })
+
+app.listen(8000)
+console.log('server running on 8000')
